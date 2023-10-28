4afc15be99c483bf9c21d8f8f4783ab2794b8857
tungjav <tungchuyenthainguyen@gmail.com>
2023-10-28 10:41:49 +0700

    get all exercise

--- web/routes/exerciseRoute.js
+++ web/routes/exerciseRoute.js
@@ -1,8 +1,13 @@
 const { Router } = require("express");
-const { add_exercise, get_exercise } = require("../controllers/exerciseController");
+const {
+  add_exercise,
+  get_exercise,
+  get_all_exercise,
+} = require("../controllers/exerciseController");
 const router = Router();
 
 router.post("/addexercise", add_exercise);
 router.get("/getexercise/:exerciseID", get_exercise);
+router.get("/getallexercise", get_all_exercise);
 
 module.exports = router;
