342e33d5be7d2e3ed0dfd3187dee776f7d6e4e76
tungctn <78474449+tungctn@users.noreply.github.com>
2023-12-09 23:59:59 +0700

    Update createToken.js

--- web/utils/createToken.js
+++ web/utils/createToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const maxAge = 3 * 24 * 60 * 60;
 module.exports.createToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET_KEY, {
+  return jwt.sign({ id }, "auth_secret", {
     expiresIn: maxAge,
   });
 };
